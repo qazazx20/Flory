1f44b3e2b751040987e05220f364a05984491da1
qazazx20 <qazazx20@daum.net>
2023-06-21 04:40:53 +0900

    main catagory

---
+++ js/index.js
@@ -0,0 +1,56 @@
+/** 슬라이드 컨테이너 */
+const container = document.querySelector('.slideContainer');
+
+/** 슬라이드 이미지 */
+let slideImg = document.querySelectorAll('.slide img');
+
+prev = document.querySelector('.prev'); // 이전 버튼
+next = document.querySelector('.next'); // 다음 버튼
+
+/** 초기화 */
+let counter = 0;
+
+// 다음
+next.addEventListener('click', slideNext);
+function slideNext() {
+    slideImg[counter].style.animation = 'next1 1s ease-out forwards';
+    if (counter >= slideImg.length - 1) {
+        counter = 0;
+    }
+    else {
+        counter++;
+    }
+    slideImg[counter].style.animation = 'next2 1s ease-out forwards';
+}
+
+// 이전
+prev.addEventListener('click', slidePrev);
+function slidePrev() {
+    slideImg[counter].style.animation = 'prev1 1s ease-out forwards';
+    if (counter == 0) {
+        counter = slideImg.length - 1;
+    }
+    else {
+        counter--;
+    }
+    slideImg[counter].style.animation = 'prev2 1s ease-out forwards';
+}
+
+
+// 자동 슬라이딩
+function autoSliding() {
+    delteInterval = setInterval(timer, 2000);
+    function timer() {
+        slideNext();
+    }
+}
+
+autoSliding();
+
+// 마우스 올렸을 때 자동 멈추기
+container.addEventListener('mouseover', () => {
+    clearInterval(delteInterval);
+});
+
+// 마우스 빠졌을 때 재개
+container.addEventListener('mouseout', autoSliding);
